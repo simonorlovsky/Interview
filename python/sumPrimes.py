7748abc9a56f809ea1540717fb7838a128bc4f27
Simon Orlovsky <simon@network-perception.com>
2016-12-16 23:57:17 -0600

    vim practice

--- python/sumPrimes.py
+++ python/sumPrimes.py
@@ -1,4 +1,8 @@
-def sumPrimes(n):
-    print("hello world")
+import functools
+import operator
 
+def sumPrimes(n):
+    lst = range(n)
+    for i in range(len(lst)):
+        filter(lambda x: x % lst[0] == 0, lst)
 sumPrimes(4)
