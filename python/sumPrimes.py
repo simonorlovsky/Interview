2bdd76b61220dcb1a7266d7b93de6e2548bb61b3
Simon Orlovsky <simon.orlovsky@gmail.com>
2016-12-17 17:09:02 -0600

    sum primes and vim practice

--- python/sumPrimes.py
+++ python/sumPrimes.py
@@ -1,8 +1,17 @@
-timport functools
+import functools
 import operator
 
+
 def sumPrimes(n):
-    lst = range(n)
-    for i in range(len(lst)):
-        filter(lambda x: x % lst[0] == 0, lst)
-sumPrimes(4)
+    lst = range(1, n)
+    notPrime = []
+    for i in range(len(lst)): 
+        for j in range(len(lst)):
+            if lst[i] % lst[j] == 0 and lst[i] != lst[j] and lst[j] != 1:
+                notPrime.append(lst[i])
+    for i in range(len(notPrime)):
+        if notPrime[i] in lst:
+            lst.remove(notPrime[i])
+    return functools.reduce(operator.add, lst)
+
+print(sumPrimes(10))
