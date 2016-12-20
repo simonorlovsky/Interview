073c96a56063e9b62939b6ece61e1f8e4ae37a29
Simon Orlovsky <simon.orlovsky@gmail.com>
2016-12-19 19:42:32 -0600

    started working on subset

---
+++ python/subset.py
@@ -0,0 +1,28 @@
+# simon
+# simo
+# sim
+# si
+# s
+
+# imon
+# imo
+# im
+# i
+
+# mon
+# mo
+# m
+
+# on
+# o
+
+# n
+
+
+def subset(str):
+    subsets = []
+    for i in range(len(str)):
+        copy = str[i:]
+        for j in range(len(str)):
+            subsets.append(copy[(len(copy)-1):]
+    return subsets
